68ab135f9c7be91bfad24b52996b449b1056a7c1
Hsuancheng Cheng <poligen@gmail.com>
2017-09-28 16:21:19 +0800

    finish sum-of-multiples

--- sum-of-multiples/sum_of_multiples.exs
+++ sum-of-multiples/sum_of_multiples.exs
@@ -4,6 +4,17 @@ defmodule SumOfMultiples do
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
+    factors_list = for n <-  1..(limit-1), is_factors(n, factors), do: n
+    Enum.sum(factors_list)
+  end
 
+
+  defp is_factors(_number, []), do: false
+  defp is_factors(number, [head | tail]) do
+    if rem(number, head) == 0 do
+      true
+    else
+      is_factors(number, tail)
+    end
   end
 end
