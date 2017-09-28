d09f1349c63ba533b3939aa01049f7a1dcef40e2
Hsuancheng Cheng <poligen@gmail.com>
2017-09-28 15:26:36 +0800

    refactor sublist

--- sublist/sublist.exs
+++ sublist/sublist.exs
@@ -3,27 +3,38 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
+  # Finished in 2.1 seconds (0.1s on load, 1.9s on tests)
+  # * test huge sublist not in huge list (1302.2ms)
+  # * test superlist early in huge list (215.3ms)
+  # * test sublist early in huge list (212.6ms)
+  # * test comparing massive equal lists (207.1ms)
+
+  def compare(a, a), do: :equal
+  def compare(a, b) when length(b) > length(a) do
+    if contains?(b, a) do
+      :sublist
+    else
+      :unequal
+    end
+  end
+  def compare(a, b) when length(a) == length(b), do: :unequal
   def compare(a, b) do
-    cond do
-      a === b -> :equal
-      contains?(a, b) -> :superlist
-      contains?(b, a) -> :sublist
-      true -> :unequal
+    if contains?(a, b) do
+      :superlist
+    else
+      :unequal
     end
   end
 
-  defp contains?(list, sublist) do
+  defp contains?(list, sublist) when length(sublist) > length(list), do: false
+  defp contains?([_ | tail ] = list, sublist) do
     sub_count = length(sublist)
-    list_count = length(list)
-
-    cond do
-      sub_count > list_count ->
-        false
-      Enum.take(list, sub_count) === sublist ->
-        true
-      true ->
-        [_ | t] = list
-        contains?(t, sublist)
+    if Enum.take(list, sub_count) === sublist do
+      true
+    else
+      contains?(tail, sublist)
     end
   end
+
 end
+
